c3b8dd201fe6625aa2d076269ae05794e6734d3e
Mykola Ts <tsybulskiyk@gmail.com>
2026-03-24 13:44:41 +0200

    Updated task-74

--- js/task-74.js
+++ js/task-74.js
@@ -5,16 +5,39 @@
 // All inputs will be valid.
 // Passing [2, 6, 3] should return 18, the product of [6, 3].
 
+// Disclaimer: only accepts solutions that are faster than his, which has a running time O(nlogn).
+
 // maxProduct([2, 1, 5, 0, 4, 3])              // 20
 // maxProduct([7, 8, 9])                       // 72
 // maxProduct([33, 231, 454, 11, 9, 99, 57])   // 104874
 
 function maxProduct(arr = []) {
-  const sortedArr = arr.sort((a, b) => b - a);
+  let max1 = -Infinity;
+  let max2 = -Infinity;
 
-  return sortedArr[0] * sortedArr[1];
+  for (const num of arr) {
+    if (num > max1) {
+      max2 = max1;
+      max1 = num;
+    } else if (num > max2) {
+      max2 = num;
+    }
+  }
+
+  return max1 * max2;
 }
 
 console.log(maxProduct([2, 1, 5, 0, 4, 3])); // 20
 console.log(maxProduct([7, 8, 9])); // 72
 console.log(maxProduct([33, 231, 454, 11, 9, 99, 57])); // 104874
+
+// Method2
+function maxProductMethod2(arr = []) {
+  const sortedArr = arr.sort((a, b) => b - a);
+
+  return sortedArr[0] * sortedArr[1];
+}
+
+console.log("Method2", maxProductMethod2([2, 1, 5, 0, 4, 3])); // 20
+console.log("Method2", maxProductMethod2([7, 8, 9])); // 72
+console.log("Method2", maxProductMethod2([33, 231, 454, 11, 9, 99, 57])); // 104874
